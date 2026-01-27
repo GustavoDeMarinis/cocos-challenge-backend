d9441c1f237dd9fae79f5173a0e571e8803215af
gustavodm <gustadema@gmail.com>
2026-01-26 23:15:35 -0300

    feature: (WIP) Still doing business logic in order service

--- src/api/order/order.ts
+++ src/api/order/order.ts
@@ -1,8 +1,9 @@
-import { Prisma, User } from "@prisma/client";
+import { Position, Prisma, User } from "@prisma/client";
 import prisma from "../../integrations/prisma/prisma-client";
 import { ErrorCode, ErrorResult } from "../../utils/shared-types";
 import { OrderSide, OrderStatus, OrderType } from "./order-api.types";
 import { isErrorResult } from "../../utils/exceptions";
+import { decimalToNumber } from "../../utils/calculators";
 
 type OrderToInsert = {
     userid: number;
@@ -53,11 +54,15 @@ export const insertOrder = async (
     orderToInsert: OrderToInsert
 ): Promise<OrderResult | ErrorResult> => {
 
-    const user = await prisma.user.findUnique({
-        where: { id: orderToInsert.userid },
-        include: {
-            positions: true,
+   const user = await prisma.user.findUnique({
+    where: { id: orderToInsert.userid },
+    include: {
+        positions: {
+            where: {
+                instrument_id: orderToInsert.instrumentid,
+            },
         },
+    },
     });
     if (!user) {
         return {
@@ -90,10 +95,6 @@ export const insertOrder = async (
         };
     }
 
-    const size =
-        orderToInsert.size ??
-        0;
-
     const price =
         orderToInsert.price != null
             ? Number(orderToInsert.price)
@@ -106,14 +107,14 @@ export const insertOrder = async (
         return sizeResult;
     }
 
-    const status = resolveInitialStatus(orderToInsert.type);
+    const status = resolveInitialStatus(orderToInsert, user, sizeResult, price);
 
     const order = await prisma.order.create({
         select: OrderSelect,
         data: {
             userid: orderToInsert.userid,
             instrumentid: orderToInsert.instrumentid,
-            size,
+            size: sizeResult,
             price,
             type: orderToInsert.type,
             side: orderToInsert.side,
@@ -150,142 +151,55 @@ const resolveSize = (
 };
 
 const isOrderSide = (value: string): value is OrderSide => {
-    return value === OrderSide.BUY || value === OrderSide.SELL;
+    return (
+        value === OrderSide.BUY ||
+        value === OrderSide.SELL ||
+        value === OrderSide.CASH_IN ||
+        value === OrderSide.CASH_OUT
+    );
 };
 
 const isOrderType = (value: string): value is OrderType => {
     return value === OrderType.LIMIT || value === OrderType.MARKET;
 };
 
-const hasEnoughCash = (user: User, size: number, price: number): boolean => {
-    const requiredAmount = size * price;
-    return Number(user.available_cash) >= requiredAmount;
-};
-
-const createRejectedOrder = async (
-    orderToInsert: OrderToInsert,
+const resolveInitialStatus = (
+    order: OrderToInsert,
+    user: User & { positions: Position[] },
     size: number,
     price: number
-) => {
-    return prisma.order.create({
-        select: OrderSelect,
-        data: {
-            userid: orderToInsert.userid,
-            instrumentid: orderToInsert.instrumentid,
-            size,
-            price,
-            type: orderToInsert.type,
-            side: orderToInsert.side,
-            status: OrderStatus.REJECTED,
-            datetime: new Date(),
-        },
-    });
-};
+): OrderStatus => {
+    if (
+        order.side === OrderSide.CASH_IN ||
+        order.side === OrderSide.CASH_OUT
+    ) {
+        return OrderStatus.FILLED;
+    }
 
-const resolveInitialStatus = (type: OrderType): OrderStatus => {
-    return type === OrderType.MARKET
+    if (order.side === OrderSide.BUY) {
+        const requiredCash = size * price;
+        if (decimalToNumber(user.available_cash) < requiredCash) {
+            return OrderStatus.REJECTED;
+        }
+    }
+
+    if (order.side === OrderSide.CASH_OUT) {
+        const requiredCash = size;
+
+        if (decimalToNumber(user.available_cash) < requiredCash) {
+            return OrderStatus.REJECTED;
+        }
+    }
+
+    if (order.side === OrderSide.SELL) {
+        const available = user.positions[0]?.size ?? 0;
+
+        if (available < size) {
+            return OrderStatus.REJECTED;
+        }
+    }
+
+    return order.type === OrderType.MARKET
         ? OrderStatus.FILLED
         : OrderStatus.NEW;
 };
-/* export const insertOrder = async (
-    orderToInsert: OrderToInsert
-): Promise<OrderResult | ErrorResult> => {
-
-    const user = await prisma.user.findUnique({
-        where: { id: orderToInsert.userid },
-    });
-    if (!user) {
-        return { code: ErrorCode.NotFound, message: "User Not Found" };
-    }
-
-    const instrument = await prisma.instrument.findUnique({
-        where: { id: orderToInsert.instrumentid },
-    });
-    if (!instrument) {
-        return { code: ErrorCode.NotFound, message: "Instrument Not Found" };
-    }
-
-    if (
-        orderToInsert.type === OrderType.LIMIT &&
-        orderToInsert.price == null
-    ) {
-        return {
-            code: ErrorCode.BadRequest,
-            message: "LIMIT orders require price",
-        };
-    }
-
-    // Resolve price
-    let orderPrice = 0;
-
-    if (orderToInsert.type === OrderType.MARKET) {
-        const marketData = await prisma.marketData.findFirst({
-            where: { instrumentid: orderToInsert.instrumentid },
-            orderBy: { date: Prisma.SortOrder.desc },
-        });
-
-        if (!marketData) {
-            return {
-                code: ErrorCode.BadRequest,
-                message: "No market data available",
-            };
-        }
-
-        orderPrice = Number(marketData.close);
-    } else {
-        orderPrice = Number(orderToInsert.price);
-    }
-
-    const requiredCash = orderToInsert.size * orderPrice;
-
-    // BUY validation
-    if (orderToInsert.side === OrderSide.BUY) {
-        if (Number(user.available_cash) < requiredCash) {
-            return prisma.order.create({
-                select: OrderSelect,
-                data: {
-                    ...orderToInsert,
-                    price: orderPrice,
-                    status: OrderStatus.REJECTED,
-                    datetime: new Date(),
-                },
-            });
-        }
-    }
-
-    // SELL validation
-    if (orderToInsert.side === OrderSide.SELL) {
-        const position = await prisma.order.aggregate({
-            where: {
-                userid: orderToInsert.userid,
-                instrumentid: orderToInsert.instrumentid,
-                status: OrderStatus.FILLED,
-            },
-            _sum: { size: true },
-        });
-
-        const availableSize = Number(position._sum.size ?? 0);
-
-        if (availableSize < orderToInsert.size) {
-            return prisma.order.create({
-                select: OrderSelect,
-                data: {
-                    ...orderToInsert,
-                    price: orderPrice,
-                    status: OrderStatus.REJECTED,
-                    datetime: new Date(),
-                },
-            });
-        }
-    }
-
-    return prisma.order.create({
-        select: OrderSelect,
-        data: {
-            ...orderToInsert,
-            price: orderPrice,
-            status: resolveInitialStatus(orderToInsert.type),
-            datetime: new Date(),
-        },
-    });
-}; */
